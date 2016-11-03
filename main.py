3211e9bea8e9ea7a9c5046f3507cc06d83e9210e
Arthur Papailhau <papailhau@gmail.com>
2016-11-03 13:04:24 +0100

    add dummy database

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-from bs4 import BeautifulSoup
 import os
+import config
+
+from bs4 import BeautifulSoup
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
-import config
+
+def insert(document_name, words, database):
+    for word in words:
+        if word in database:
+            database[word].append(document_name)
+        else:
+            database[word] = [document_name]
+    print(document_name)
+    print(words)
+    print(database)
+    print(" ")
+
 
 def parser(file_content_raw):
     soup = BeautifulSoup(file_content_raw, config.TYPE_PARSER)
@@ -11,14 +24,17 @@ def parser(file_content_raw):
     tokenizer = RegexpTokenizer(r'\w+')
     stop = stopwords.words(config.LANG)
     words = [i for i in tokenizer.tokenize(text) if i not in stop]
-    print(list(set(words)))
+    #print(list(set(words)), end="\n\n")
+    return words
 
-def indexer():
+def indexer(database):
     directory = config.DIRECTORY
     for file in os.listdir(directory):
         if file.endswith(config.EXTENSION):
             f = open(directory+file, 'r')
-            parser(f.read())
+            words = parser(f.read())
+            insert(os.path.basename(f.name), words, database)
 
 if __name__ == '__main__':
-    indexer()
+    db = {}
+    indexer(db)
