2eb2d5f60cf5658b25e798471c6cf446b7054859
Arthur Papailhau <papailhau@gmail.com>
2016-11-16 19:07:41 +0100

    add multi words search

--- routes/indexor.js
+++ routes/indexor.js
@@ -3,35 +3,49 @@ var router = express.Router();
 var db = require('../db/db');
 var exec = require('child_process').exec;
 
-router.get('/', function(req, res, next) {var collection = db.get().collection('words');
+router.get('/', function (req, res, next) {
+  var collection = db.get().collection('words');
   // Call a python script in order to stemm and remove_stop_word from the input sentence
   var sentence = req.query.words;
-  var cmd = 'python3 indexer/stem_me.py '+sentence;
+  var cmd = 'python3 indexer/stem_me.py ' + sentence;
   var p = new Promise(
     (resolve, error) => {
-      exec(cmd, function(err, stdout, stderr) {
+      exec(cmd, function (err, stdout, stderr) {
         if (err) error(err);
         resolve(stdout);
       });
     }
   ).then((res) => {
-    // TODO: for loop on each words, wait for the result of async_find, at the end return the array of Documents. (sorted by occurence)
-    // check my nlc-manager.js
-    async_find(res);
-  })
+    var arrayOfPromises = [];
+    var sentenceStemmed = res.split(" ");
+    for (var i = 0; i < sentenceStemmed.length; i++) {
+      var findPromise = async_find(sentenceStemmed[i]);
+      arrayOfPromises.push(findPromise);
+      console.log("word: " + sentenceStemmed[i]);
+    }
+    return Promise.all(arrayOfPromises);
+  }).then(result => {
+    console.log("result: " + result);
+    res.render('documents', { documents: result});
+  });
 
   function async_find(word) {
-    collection.find({'_id': word}).toArray(function(err, result) {
-    console.log('result: '+JSON.stringify(result));
-    if (!Object.keys(result).length) {
-      res.render('documents', { documents: ['empty']});
-    } else {
-      var documents = result[0].documents;
-      console.log(documents);
-      res.render('documents', { documents: documents});
-    }
-  });
-  };
+    return new Promise(
+      (resolve, error) => {
+        collection.find({ '_id': word }).toArray(function (err, result) {
+          if (err) error(err);
+          //console.log('result: '+JSON.stringify(result));
+          if (!Object.keys(result).length) {
+            resolve([]);
+          } else {
+            var documents = result[0].documents;
+            console.log(documents);
+            resolve(documents);
+          }
+        });
+      }
+    );
+  }
 
 });
 module.exports = router;
