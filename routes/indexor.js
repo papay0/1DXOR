f04abe188ec9eece59d049df47d64c37b37267d1
Arthur Papailhau <papailhau@gmail.com>
2016-11-16 19:38:59 +0100

    improved result, sort by words frequency

--- routes/indexor.js
+++ routes/indexor.js
@@ -8,13 +8,12 @@ router.get('/', function (req, res, next) {
   // Call a python script in order to stemm and remove_stop_word from the input sentence
   var sentence = req.query.words;
   var cmd = 'python3 indexer/stem_me.py ' + sentence;
-  var p = new Promise(
-    (resolve, error) => {
-      exec(cmd, function (err, stdout, stderr) {
-        if (err) error(err);
-        resolve(stdout);
-      });
-    }
+  var p = new Promise((resolve, error) => {
+    exec(cmd, function (err, stdout, stderr) {
+      if (err) error(err);
+      resolve(stdout);
+    });
+  }
   ).then((res) => {
     var arrayOfPromises = [];
     var sentenceStemmed = res.split(" ");
@@ -25,21 +24,36 @@ router.get('/', function (req, res, next) {
     }
     return Promise.all(arrayOfPromises);
   }).then(result => {
-    console.log("result: " + result);
-    res.render('documents', { documents: result});
+    var resultMerged = [].concat.apply([], result);
+    console.log("resultMerged: "+resultMerged);
+    console.log("result: " + sortByFrequency(resultMerged));
+    res.render('documents', { documents: sortByFrequency(resultMerged) });
   });
+
+  function sortByFrequency(array) {
+    var frequency = {};
+
+    array.forEach(function (value) { frequency[value] = 0; });
+
+    var uniques = array.filter(function (value) {
+      return ++frequency[value] == 1;
+    });
+
+    return uniques.sort(function (a, b) {
+      return frequency[b] - frequency[a];
+    });
+  }
 
   function async_find(word) {
     return new Promise(
       (resolve, error) => {
         collection.find({ '_id': word }).toArray(function (err, result) {
           if (err) error(err);
-          //console.log('result: '+JSON.stringify(result));
           if (!Object.keys(result).length) {
             resolve([]);
           } else {
             var documents = result[0].documents;
-            console.log(documents);
+            console.log("res find: "+documents);
             resolve(documents);
           }
         });
